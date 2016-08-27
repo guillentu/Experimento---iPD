be6dfef6de634baa485a4937adad393aaa0c22b1
Guillermo Delmas <guille@iibm>
2016-08-26 21:11:41 -0300

    guille commit 26/08/2016

--- ExtraerDatos/graficos_iPD_1_2_9s_13s_primeroEnPalanquear.m
+++ ExtraerDatos/graficos_iPD_1_2_9s_13s_primeroEnPalanquear.m
@@ -4,7 +4,7 @@ clear all
 close all
 %load "iPD_1_2_9s_13s/datosCargadoWorkspace20160423";
 load "iPD_1_2_9s_13s/datos_errores_y_tiempos"
-
+_txtSujetos=["1A";"2A";"3A";"4A";"5A";"6A";"7A";"8A";"9A";"10A";"3B";"4B"];
 indice=["exp00";"exp01";"exp02";"exp03";"exp04";"exp05";
         "exp06";"exp07";"exp08";"exp09";"exp10";"exp11";
         "exp12";"exp13";"exp14";"exp15";"exp16";"exp17";
@@ -53,12 +53,12 @@ endfor
 % tiempo ventanas + derecha(opo C) - izquierda (opo D)
 
 %Cantidad  de veces que presiono´ primero la palanca
-sincro=[]
+sincro=zeros(5,nfields(todo),_nSujetos);
 for i=1:_nSujetos
   for j=1:expXsuj(i)
     contador_1ro=0;contador_2do=0;contador_igual=0;
+    auxFallas=1;_contador_1ro=0;_contador_igual=0;_contador_2do=0;_error=0;_alterna=0;
     for k=_trialIni:_trialFin
-      
       if ((todo.(indice(j+1,:))(i)._respuestasEXP(k)==0)||(todo.(indice(j+1,:))(i)._respuestasOPO(k)==0))
         if (k==1)
         elseif (k==2) 
@@ -77,25 +77,69 @@ for i=1:_nSujetos
           auxFallas+=1;
         endif
       else
-        if todo.(indice(j+1,:))(i)._respuestasEXP(k-auxFallas)==2
+        if (k==1)||(todo.(indice(j+1,:))(i)._respuestasEXP(k-auxFallas)==2)
           auxOPO=max(todo.(indice(j+1,:))(i)._ventanas2(k,:));% primer palanqueo correcto
           auxEXP=todo.(indice(j+1,:))(i)._reloj(2,k);
-        else todo.(indice(j+1,:))(i)._respuestasEXP(k-auxFallas)==1
-          auxOPO=min(todo.(indice(j+1,:))(i)._ventanas2(k,:));% primer palanqueo correcto
+          auxOPOmin=min(todo.(indice(j+1,:))(i)._ventanas2(k,:));
+          if (auxOPOmin < 0)
+            if abs(auxOPOmin) > auxEXP
+              _alterna++;
+            endif
+          endif
+        else (todo.(indice(j+1,:))(i)._respuestasEXP(k-auxFallas)==1)
+          auxOPO=abs(min(todo.(indice(j+1,:))(i)._ventanas2(k,:)));% primer palanqueo correcto
           auxEXP=todo.(indice(j+1,:))(i)._reloj(2,k);
         endif
         if auxEXP > auxOPO % Experimental palanqueo primero
-          contador_1ro++;
-        elseif auxEXP = auxOPO
-          contador_igual++;
+          _contador_1ro++;
+        elseif auxEXP == auxOPO
+          _contador_igual++;
         elseif auxEXP < auxOPO
-          contador_2do++;
+          _contador_2do++;
         else
-          error++;
+          _error++;
         endif
         auxFallas=1;
-      else
-        a="MIERDA"
       endif
     endfor
-    sincro.(indiceSujeto(i,:)).(indice(j+1,:))=[contador_1ro;contador_igual;contador_2do;error];
+    %sincro.(indiceSujeto(i,:)).(indice(j+1,:))=[_contador_1ro;_contador_igual;_contador_2do;_error];
+    sincro(:,j,i)=[_contador_1ro;_contador_igual;_contador_2do;_error;_alterna];
+  endfor
+endfor
+  
+%%%% TODOS JUNTOS
+%figure();hold on;
+%for i=1:_nSujetos
+%  inicioAux=1;%expXsuj(i)-_ultimosX+1;
+%  finAux=expXsuj(i);
+%  h=plot([inicioAux:finAux],sincro(1,inicioAux:finAux,i),_colores(i,:));
+%  set(h, "linewidth", 2);
+%  xlabel("n de sesiones");
+%  ylabel("trials que presiono´ primero");
+%  title("Cantidad de trial por Sesion donde presiono primero"); 
+% grid on;
+%endfor
+%legend(_txtSujetos(:,:),4);
+%hold off;
+
+%%%% DE A UNO
+for i=1:_nSujetos
+  figure();
+  inicioAux=1;%expXsuj(i)-_ultimosX+1;
+  finAux=expXsuj(i);
+  h=plot([inicioAux:finAux],sincro(1,inicioAux:finAux,i) + sincro(2,inicioAux:finAux,i),_colores(2,:),
+         [inicioAux:finAux],sincro(3,inicioAux:finAux,i),_colores(3,:),
+         [inicioAux:finAux],sincro(4,inicioAux:finAux,i),_colores(4,:),
+         [inicioAux:finAux],sincro(1,inicioAux:finAux,i) + sincro(2,inicioAux:finAux,i) +
+                            sincro(3,inicioAux:finAux,i) + sincro(4,inicioAux:finAux,i),_colores(5,:),
+         [inicioAux:finAux],sincro(5,inicioAux:finAux,i),_colores(6,:));
+  set(h, "linewidth", 2);
+  xlabel("n de sesiones");
+  ylabel("palanqueos por trials");
+  title(strcat("Cantidad de trial por Sesion don presiono primero. Sujeto: ",_txtSujetos(i,:))); 
+  legend("palanqueo 1ro + igual","palanqueo 2do","errores","Total","Alterna");
+  grid on;
+  axis ([0,expXsuj(i)+12,0,31], "square");
+endfor
+
+  
