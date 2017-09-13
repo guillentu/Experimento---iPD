6dc5f0576fc63b608ac59bf62442187c6c7d7c6d
Guillermo Delmas <guille@iibm>
2017-09-12 22:58:46 -0300

    guille commit 12/09/2017 figuras 2  poblaciones

---
+++ ExtraerDatos/mesada5.m
@@ -0,0 +1,60 @@
+mediaXsujeto_T4=_mediaXsujeto;
+timeOutMedia_T4=_timeOutMedia;
+foodMedia_T4=foodMedia;
+promediosC_T4=_promediosC;
+timeOutSem_T4=_timeOutSem;
+foodSem_T4=foodSem;
+semXsujeto_T4=_semXsujeto;
+timeOut_T4=_timeOut;
+food_T4=food;
+
+mediaXsujeto_T3=_mediaXsujeto;
+timeOutMedia_T3=_timeOutMedia;
+foodMedia_T3=foodMedia;
+promediosC_T3=_promediosC;
+timeOutSem_T3=_timeOutSem;
+foodSem_T3=foodSem;
+semXsujeto_T3=_semXsujeto;
+timeOut_T3=_timeOut;
+food_T3=food;
+
+mediaXsujeto_T2=_mediaXsujeto;
+timeOutMedia_T2=_timeOutMedia;
+foodMedia_T2=foodMedia;
+promediosC_T2=_promediosC;
+timeOutSem_T2=_timeOutSem;
+foodSem_T2=foodSem;
+semXsujeto_T2=_semXsujeto;
+timeOut_T2=_timeOut;
+food_T2=food;
+
+save('-mat7-binary','datos_coop_reward_T2.mat','mediaXsujeto_T2','timeOutMedia_T2','foodMedia_T2','promediosC_T2',...
+         'timeOutSem_T2','foodSem_T2','semXsujeto_T2','timeOut_T2','food_T2');
+save('datos_coop_reward_T2','mediaXsujeto_T2','timeOutMedia_T2','foodMedia_T2','promediosC_T2',...
+         'timeOutSem_T2','foodSem_T2','semXsujeto_T2','timeOut_T2','food_T2');
+
+save('-mat7-binary','datos_coop_reward_T3.mat','mediaXsujeto_T3','timeOutMedia_T3','foodMedia_T3','promediosC_T3',...
+         'timeOutSem_T3','foodSem_T3','semXsujeto_T3','timeOut_T3','food_T3');
+save('datos_coop_reward_T3','mediaXsujeto_T3','timeOutMedia_T3','foodMedia_T3','promediosC_T3',...
+         'timeOutSem_T3','foodSem_T3','semXsujeto_T3','timeOut_T3','food_T3');
+         
+save('-mat7-binary','datos_coop_reward_T4.mat','mediaXsujeto_T4','timeOutMedia_T4','foodMedia_T4','promediosC_T4',...
+         'timeOutSem_T4','foodSem_T4','semXsujeto_T4','timeOut_T4','food_T4');
+save('datos_coop_reward_T4','mediaXsujeto_T4','timeOutMedia_T4','foodMedia_T4','promediosC_T4',...
+         'timeOutSem_T4','foodSem_T4','semXsujeto_T4','timeOut_T4','food_T4');
+         
+         
+R2_T2=R2;
+R_mean_T2=R_mean;
+save('-mat7-binary','datos_coopmutua_T2.mat','R2_T2','R_mean_T2');
+save('datos_coopmutua_T2','R2_T2','R_mean_T2');
+         
+R2_T3=R2;
+R_mean_T3=R_mean;
+save('-mat7-binary','datos_coopmutua_T3.mat','R2_T3','R_mean_T3');
+save('datos_coopmutua_T3','R2_T3','R_mean_T3');
+
+R2_T4=R2;
+R_mean_T4=R_mean;
+save('-mat7-binary','datos_coopmutua_T4.mat','R2_T4','R_mean_T4');
+save('datos_coopmutua_T4','R2_T4','R_mean_T4');
